457755d8e5da6df9d058b1f0e5c43a95ba7752cf
Artyom Babayan <114479248+tema-git@users.noreply.github.com>
2023-04-13 00:11:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
+chat_id = 401141478
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+    sl = 0.01
+    pval = mannwhitneyu(x, y, alternative="less").pvalue
+    
+    return pval < sl
