8b608769e253aa1166609acfa6105aeaa1c7c721
Artyom Babayan <114479248+tema-git@users.noreply.github.com>
2023-04-13 15:32:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ from scipy.stats import mannwhitneyu
 chat_id = 401141478
 
 def solution(x: np.array, y: np.array) -> bool:
-    sl = 0.01
+    sl = 0.09
     pval = mannwhitneyu(x, y, alternative="less").pvalue
     
     return pval < sl
